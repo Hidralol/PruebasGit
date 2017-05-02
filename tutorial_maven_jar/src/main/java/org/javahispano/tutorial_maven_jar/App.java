afdcf24cbfda28b2607958ccd0c5ebf2b0cef5b8
Hidra <artic4_6@hotmail.com>
2017-05-02 19:07:50 +0200

    version inicial de proyecto

---
+++ tutorial_maven_jar/src/main/java/org/javahispano/tutorial_maven_jar/App.java
@@ -0,0 +1,13 @@
+package org.javahispano.tutorial_maven_jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
